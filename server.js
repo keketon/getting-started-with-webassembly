880a50b8ee2a875eadcb33ab0200218c6b661fef
keketon <mas08rin10yabko@gmail.com>
2023-10-14 10:43:01 +0900

    import/export functions and the custom memory

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("public"));
+app.use(
+  express.static("public", {
+    // Not required for the current version of the express but added for my learning
+    setHeaders: (res, path, stat) => {
+      if (path.endsWith(".wasm")) {
+        res.set("Content-Type", "application/wasm");
+      }
+    },
+  })
+);
 
 app.listen(2222, () => console.log("Server started on port 2222!"));
